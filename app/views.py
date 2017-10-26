ee09cbfbbc7d9e148cfca679975cdb8093c9ac72
李利锋 <lilifeng@meican-inc.com>
2017-10-26 14:43:01 +0800

    Initial commit

---
+++ app/views.py
@@ -0,0 +1,13 @@
+from app import app
+from flask import render_template
+
+@app.route('/')
+@app.route('/index')
+def index():
+	user = {'nickname': 'Miguel'}  # fake user
+	return render_template("index.html",
+						   title='Home',
+						   user=user)
+@app.route('/login')
+def login():
+	return "login"
